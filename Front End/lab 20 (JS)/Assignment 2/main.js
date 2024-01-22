cb6c79d0394a5c741fb114790e717a2588d1b782
Mohamed234 <mohamedibrahimy911@gmail.com>
2024-01-23 00:59:33 +0200

    upload file

---
+++ Front End/lab 20 (JS)/Assignment 2/main.js
@@ -0,0 +1,52 @@
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+
+
+function scaleOne(){
+    one.style.width = "800px"
+    one.style.transition = "0.5s"
+    two.style.width = "60px"
+    two.style.transition = "0.5s"
+    three.style.width = "60px"
+    three.style.transition = "0.5s"
+    four.style.width = "60px"
+    four.style.transition = "0.5s"
+
+}
+function scaleTwo(){
+    one.style.width = "60px"
+    one.style.transition = "0.5s"
+    two.style.width = "800px"
+    two.style.transition = "0.5s"
+    three.style.width = "60px"
+    three.style.transition = "0.5s"
+    four.style.width = "60px"
+    four.style.transition = "0.5s"
+
+}
+function scaleThree(){
+    one.style.width = "60px"
+    one.style.transition = "0.5s"
+    two.style.width = "60px"
+    two.style.transition = "0.5s"
+    three.style.width = "800px"
+    three.style.transition = "0.5s"
+    four.style.width = "60px"
+    four.style.transition = "0.5s"
+
+}
+function scaleFour(){
+    one.style.width = "60px"
+    one.style.transition = "0.5s"
+    two.style.width = "60px"
+    two.style.transition = "0.5s"
+    three.style.width = "60px"
+    three.style.transition = "0.5s"
+    four.style.width = "800px"
+    four.style.transition = "0.5s"
+
+}
+
+
